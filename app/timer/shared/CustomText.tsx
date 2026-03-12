edc86fa20bde3e309268f28a23b98a01f7397351
Danica Velarde <velarded@Danicas-MacBook-Pro.local>
2026-03-11 20:23:20 -0400

    replace JSX with Node

--- app/timer/shared/CustomText.tsx
+++ app/timer/shared/CustomText.tsx
@@ -4,16 +4,18 @@ import React from "react";
 
 type Props = React.HTMLAttributes<HTMLElement> & {
   style?: React.CSSProperties;
-  as?: keyof JSX.IntrinsicElements;
+  as?: React.ElementType;
   children?: React.ReactNode;
 };
 
 export default function CustomText({ style, as: Component = "span", children, ...props }: Props) {
   const baseStyle: React.CSSProperties = { fontFamily: "VT323", display: "inline-block" };
+  const Tag = Component as React.ElementType;
   return (
-    // @ts-ignore allow spreading generic html props
-    <Component style={{ ...baseStyle, ...style }} {...props}>
+    // Tag is a valid React element type (string or component)
+    // @ts-ignore allow spreading generic html props when Tag is dynamic
+    <Tag style={{ ...baseStyle, ...style }} {...props}>
       {children}
-    </Component>
+    </Tag>
   );
 }
